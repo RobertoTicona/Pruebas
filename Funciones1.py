951060cce40390d3337e4a4c959a09fdf64be095
Roberto <angeldiablitotkd66@gmail.com>
2025-09-06 12:43:12 -0500

    Optimizando el reconocedor de funciones

--- Funciones1.py
+++ Funciones1.py
@@ -11,16 +11,27 @@ len() devuelve la longitud (cuántos elementos hay) en listas, cadenas, conjunto
 
 expresion = input("Ingresa una función matemática: ")
 
-variables = set()        # para guardar letras distintas
+variables = set()
 operaciones = 0
 
-for c in expresion:
-    if c.isalpha():      # si es letra, es variable
+for i, c in enumerate(expresion):
+    if c.isalpha():
         variables.add(c)
-    elif c in "+-":      # si es + o -, lo contamos
+
+    elif c in "+-*/^":
         operaciones += 1
+
+    # Detectar multiplicación implícita
+    if i < len(expresion) - 1:  # mientras no sea el último carácter
+        siguiente = expresion[i+1]
+        # caso: número + letra  (ej: 5x)
+        if c.isdigit() and siguiente.isalpha():
+            operaciones += 1
+        # caso: letra + letra (ej: xy)
+        elif c.isalpha() and siguiente.isalpha():
+            operaciones += 1
 
 print("\n--- RESULTADOS ---")
 print("Variables encontradas:", ", ".join(sorted(variables)))
 print("Número de variables:", len(variables))
-print("Número de sumas/restas:", operaciones)
+print("Número de operaciones registradas:", operaciones)
